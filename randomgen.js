b58d6cbaef13daa29a551c95b9b7d770f8b88629
Nkosinice <nkosinathimatutu.work@gmail.com>
2022-07-17 23:22:30 +0200

    Added Password Randomization Logic & updated CSS

--- randomgen.js
+++ randomgen.js
@@ -4,3 +4,38 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 "!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+
+const inputFld = document.getElementById("inpt")
+const genBtn = document.getElementById("gen")
+let pswrda = document.getElementById("b1")
+const pswrdb = document.getElementById("b2")
+const pswrdc = document.getElementById("b3")
+let val =[]
+let value = ""
+
+function random () {
+  
+    value = ""
+    for(let i = 0 ; i < 11; i++){
+      
+
+
+        value += characters[Math.floor(Math.random()*90)]
+
+    }
+    console.log(value)
+    val.push(value)
+}
+
+genBtn.addEventListener('click', function () {
+    console.log("clicked")
+    
+     
+     random()
+     pswrda.textContent = value
+     random()
+     pswrdb.textContent = value
+     random()
+     pswrdc.textContent = value
+}
+)
